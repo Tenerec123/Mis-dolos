cd3d2082cf03d6457264ace6f9920f4a9c0c9b05
Tenerec123 <alvarofer8a@gmail.com>
2025-08-06 15:33:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+const Slides = document.querySelectorAll('.slide');
+let slideIdx = 0;
+let intervalId = null;
 
+document.addEventListener('DOMContentLoaded', initSlider)
+
+function initSlider(){
+    if (Slides.length > 0){
+        Slides[slideIdx].classList.add('display');
+    }
+   
+}
+function ShowSlide(beforeIdx, newIdx){
+    Slides[beforeIdx].classList.remove('display');
+    Slides[newIdx].classList.add('display');
+    slideIdx = newIdx
+}
+function Prev(){
+    ShowSlide(slideIdx, (slideIdx + 15 )% 4);
+}
+function Next(){
+    ShowSlide(slideIdx, (slideIdx +1) % 4)
+}
